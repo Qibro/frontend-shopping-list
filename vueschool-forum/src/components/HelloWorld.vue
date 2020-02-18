1d1fe6b5ef9d4438caa6f278466b322605704c09
Fadli Hidayatullah <fadli.hidayatullah@kumparan.com>
2020-02-18 19:29:14 +0700

    initial commit
    Signed-off-by: Fadli Hidayatullah <fadli.hidayatullah@kumparan.com>

---
+++ vueschool-forum/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Welcome to the Forum</h1>
+    <div v-for="thread in threads">
+      <h2>{{thread.title}}</h2>
+      <div v-for="postId in thread.posts">
+        <p>{{users[posts[postId].userId].name}}</p>
+        <p>{{posts[postId].text}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import dataSource from '@/data.json'
+console.log(dataSource)
+export default {
+  data () {
+    return {
+      threads: dataSource.threads,
+      posts: dataSource.posts,
+      users: dataSource.users
+    }
+  }
+}
+</script>
